0318229aa187bc1f9fb6835d12afc8ce13d8ea66
Moonjunwoong <myc0611@naver.com>
2023-11-27 05:49:42 +0900

    feat : 데이터베이스 연동, html확인 남음

--- src/main/java/com/example/HomeAutomation_Project/SettingController.java
+++ src/main/java/com/example/HomeAutomation_Project/SettingController.java
@@ -6,7 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/settings")
+@RequestMapping("/settings/settings" +
+        "")
 public class SettingController {
 
     @GetMapping("/settings")
